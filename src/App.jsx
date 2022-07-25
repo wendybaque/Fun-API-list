e51df1e5c220a5e7f23e9b1abeeb754d6e6282a3
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-25 14:23:47 +0200

    Add Footer component with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import './App.css';
+import Footer from "../src/components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Footer />
     </div>
   );
 }
